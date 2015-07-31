2329896d301faf2c30ef234e6390b3450189465d
Daniel J Montanez <dmontanez86@gmail.com>
2015-07-30 21:00:30 -0700

    Small Size Sub Nav
    UX for small screens - sub nav tabs change to dropdown menu.

--- js/content.query.js
+++ js/content.query.js
@@ -2,4 +2,23 @@ $(document).ready(function() {
     $(".nav-tabs li a").on('click', function(event) {
         event.preventDefault();
     });
+    
+    $(".dropdown-menu li a").on('click', function(event) {
+        event.preventDefault();
+    });
+    
+    var w = 1000;
+    
+    $(window).on("load resize", function(event){
+        w = $(window).width();
+        if( w < 500) {
+            $(".nav-tabs").hide();
+            $(".small").show();
+        }
+        if (w > 500) {
+            $(".small").hide();
+            $(".nav-tabs").show();
+        }
+    });
+    
 });
